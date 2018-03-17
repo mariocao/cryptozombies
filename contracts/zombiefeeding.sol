68cbc9cd447c6865a3bad8593d87849e22d70c63
Mario Cao <mariocao@gmail.com>
2018-03-17 18:18:00 +0100

    [L5] Fix modifier OnlyOwnerOf

--- contracts/zombiefeeding.sol
+++ contracts/zombiefeeding.sol
@@ -21,8 +21,7 @@ contract ZombieFeeding is ZombieFactory {
 
   KittyInterface kittyContract;
 
-  // 1. Change modifier name to `onlyOwnerOf`
-  modifier ownerOf(uint _zombieId) {
+  modifier onlyOwnerOf(uint _zombieId) {
     require(msg.sender == zombieToOwner[_zombieId]);
     _;
   }
@@ -39,8 +38,7 @@ contract ZombieFeeding is ZombieFactory {
       return (_zombie.readyTime <= now);
   }
 
-  // 2. Change modifier name here as well
-  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) internal ownerOf(_zombieId) {
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) internal onlyOwnerOf(_zombieId) {
     Zombie storage myZombie = zombies[_zombieId];
     require(_isReady(myZombie));
     _targetDna = _targetDna % dnaModulus;
