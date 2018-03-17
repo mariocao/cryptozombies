3107e730b0cdf6f31b64cbc8198ca41c83f6b706
Mario Cao <mariocao@gmail.com>
2018-03-17 17:46:10 +0100

    [L4] Zombie level up

--- contracts/zombieattack.sol
+++ contracts/zombieattack.sol
@@ -23,7 +23,7 @@ contract ZombieBattle is ZombieHelper {
     } else {
       myZombie.lossCount++;
       enemyZombie.winCount++;
+      _triggerCooldown(myZombie);
     }
-    _triggerCooldown(myZombie);
   }
 }
